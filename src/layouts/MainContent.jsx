fd259b0438680d892e422cc8abb5d9c6386f7cc6
TheSaiTeja <iamsaiteja2411@gmail.com>
2025-01-18 17:03:00 +0530

    Update main layout styling for dark and light theme

--- src/layouts/MainContent.jsx
+++ src/layouts/MainContent.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import TodoForm from "../components/todos/TodoForm";
 import TodoList from "../components/todos/TodoList";
 import { useTodo } from "../context/TodoContext";
+import { useTheme } from "../context/ThemeContext";
 
 export default function MainContent() {
   const { isEditing } = useTodo();
+  const { theme } = useTheme();
+
+  const isDark = theme === "dark";
 
   return (
-    <div className="main md:w-5/6 md:min-h-screen md:max-h-full p-2">
-      <div className="main border-black border-2 min-h-full rounded-lg ">
+    <div
+      className={`main md:w-5/6 md:min-h-screen md:max-h-full p-2 ${
+        isDark ? "bg-gray-800" : "bg-white"
+      }`}
+    >
+      <div
+        className={`main min-h-full rounded-lg ${
+          isDark ? " border-gray-600 border-2" : "border-black border-2"
+        }`}
+      >
         {isEditing ? (
           <TodoForm />
         ) : (
